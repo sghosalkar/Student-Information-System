5be6bd6791aabf1a97a28c96942a4214ca707537
srushti.karangale <srushti.karangale@gmail.com>
2020-06-12 23:52:26 +0530

    added addStudentView

--- Home.py
+++ Home.py
@@ -1,5 +1,34 @@
 from tkinter import *
+from Views.AddView import createAddView
+
+
 root = Tk()
 root.title('S.M.S')
 root.geometry("300x600+10+20")
+def addClickAction():
+    createAddView(root)
+def viewClickAction():
+    root.withdraw()
+def UpdateClickAction():
+    root.withdraw()
+def DeleteClickAction():
+    root.withdraw()
+def GraphClickAction():
+    root.withdraw()            
+    
+
+btnAdd = Button(root, text = "Add", font=("arial", 18, 'bold'),width=10, command=addClickAction)
+btnView = Button(root, text = "View", font=("arial", 18, 'bold'),width=10, command=viewClickAction)
+btnUpdate = Button(root, text = "Update", font=("arial", 18, 'bold'),width=10, command=UpdateClickAction)
+btnDelete = Button(root, text = "Delete", font=("arial", 18, 'bold'),width=10, command=DeleteClickAction)
+btnGraph = Button(root, text = "Graph", font=("arial", 18, 'bold'),width=10, command=GraphClickAction)
+
+btnAdd.pack(padx=2, pady=10)
+btnView.pack(padx=2, pady=10)
+btnUpdate.pack(padx=2, pady=10)
+btnDelete.pack(padx=2, pady=10)
+btnGraph.pack(padx=2, pady=10)
+
+
+
 root.mainloop()
---
+++ Views/AddView.py
@@ -0,0 +1,18 @@
+from tkinter import *
+
+
+def createAddView(root):
+    addStudent = Toplevel(root)
+    addStudent.title("Add St.")
+    addStudent.geometry("500x400+200+200")
+
+
+# lblAddRno = Label(adst, text="enter rno", font=("arial", 18, 'bold'))
+# entAddRno = Entry(adst, bd=10, font=("arial", 18, 'bold'))
+# lblAddName = Label(adst, text="enter name", font=("arial", 18, 'bold'))
+# entAddName = Entry(adst, bd=10, font=("arial", 18, 'bold'))
+
+# lblAddRno.pack(pady=10)
+# entAddRno.pack(pady=10)
+# lblAddName.pack(pady=10)
+# entAddName.pack(pady=10)
